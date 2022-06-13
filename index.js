1f2527138670a0bde863276d2b146a9b84dbcfc9
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-06-13 03:49:14 +0100

    :art: add in view engine
    add view engine and modify dirname for static assets

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const app = express()
+const friendsRouter = require("./routes/friends.router")
 
+const messagesRouter = require("./routes/messages.router")
+const path = require("path")
 const PORT = 3001
+
+app.set("view engine", "hbs")
+app.set("vies", path.join("__dirname", "views"))
 
 app.use((req, res, next) => {
   const start = Date.now()
@@ -11,14 +17,18 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} ${delta}ms`)
 })
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "My Friends are very clever",
+    caption: "Let's go skiing"
+  })
+})
+
+app.use(express.static(path.join(__dirname, "public")))
+
 app.use(express.json())
 
-const friendsRouter = require("./routes/friends.router")
-
 app.use("/friends", friendsRouter)
-
-const messagesRouter = require("./routes/messages.router")
-
 app.use("/messages", messagesRouter)
 
 app.listen(PORT, () => {
